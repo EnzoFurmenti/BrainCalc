bd68940191e5d79c0f71141dc507c3015d4cd9be
EnzoFurmenti <ducheenzo@mail.ru>
2016-06-28 17:32:20 +0300

    add new function

--- BrainCalc/BrainCalc.swift
+++ BrainCalc/BrainCalc.swift
@@ -7,3 +7,66 @@
 //
 
 import Foundation
+
+class BrainCalc{
+    
+ private   enum Op {
+        case operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+    }
+   private var opStack = [Op] ()
+   private var knownOps = [String : Op]()
+
+    init (){
+        
+        knownOps["×"] = Op.BinaryOperation("×", *)
+        knownOps["÷"] = Op.BinaryOperation("÷"){$1 / $0}
+        knownOps["-"] = Op.BinaryOperation("-"){$1 - $0}
+        knownOps["+"] = Op.BinaryOperation("+", +)
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+    }
+    
+    
+    func pushOperand(operand: Double) {
+        opStack.append(Op.operand(<#T##Double#>))
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+    }
+    
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]){
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+        
+            switch op {
+            case .operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result{
+                return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result{
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+
+            }
+        }
+        return (nil, ops)
+    }
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        return result
+    }
+}
