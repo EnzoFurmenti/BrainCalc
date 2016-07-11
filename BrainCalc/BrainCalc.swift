27d8d4852ada360aac02a1dbfda3f4d0c0133146
EnzoFurmenti <ducheenzo@mail.ru>
2016-07-11 13:13:26 +0300

    add new function

--- BrainCalc/BrainCalc.swift
+++ BrainCalc/BrainCalc.swift
@@ -9,9 +9,11 @@
 import Foundation
 
 class BrainCalc{
-    
+ //MARK: -var,let,enum-
     private   enum Op : CustomStringConvertible {
         case operand(Double)
+        case variable(String, () -> Double?)
+        case operandPi(String, () -> Double)
         case UnaryOperation(String, Double -> Double)
         case BinaryOperation(String, (Double, Double) -> Double)
     
@@ -20,6 +22,10 @@ class BrainCalc{
             switch self{
             case .operand(let operand):
                 return "\(operand)"
+            case .variable(_,_):
+                return "M"
+            case .operandPi(_,_):
+                return "π"
             case .UnaryOperation(let symbol, _):
                 return symbol
             case .BinaryOperation(let symbol, _):
@@ -30,23 +36,49 @@ class BrainCalc{
     }
     }
    private var opStack = [Op] ()
-   private var knownOps = [String : Op]()
-
+   private var knownOps = [String : Op] ()
+   var variableValues = [String : Double] ()
+   var description:String?{
+        if !opStack.isEmpty{
+            var stack = opStack
+            var totalString = String ()
+            while !stack.isEmpty {
+                let subTotalString = evaluate1(stack).result!
+                totalString = subTotalString + totalString
+                stack = evaluate1(stack).remainingOps
+            }
+            return totalString
+        }
+        return " "
+    }
+//MARK: -init-
     init (){
-        func learnOp (op :Op) {
+        func learnOp (op : Op) {
             knownOps[op.description] = op
         }
         learnOp(Op.BinaryOperation("×", *))
-        knownOps["×"] = Op.BinaryOperation("×", *)
-        knownOps["÷"] = Op.BinaryOperation("÷"){$1 / $0}
-        knownOps["-"] = Op.BinaryOperation("-"){$1 - $0}
-        knownOps["+"] = Op.BinaryOperation("+", +)
-        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+        learnOp(Op.BinaryOperation("÷", {$1 / $0}))
+        learnOp(Op.BinaryOperation("-", {$1 - $0}))
+        learnOp(Op.BinaryOperation("+", +))
+        learnOp(Op.UnaryOperation("√", sqrt))
+        learnOp(Op.UnaryOperation("sin", sin))
+        learnOp(Op.UnaryOperation("cos", cos))
+        learnOp(Op.UnaryOperation("±", {-$0}))
+        learnOp(Op.operandPi("π", {M_PI}))
+        learnOp(Op.variable("M", {self.variableValues["M"]}))
+
     }
     
+//MARK: -Metods-
+       func pushOperand(symbol: String) ->Double?{
+            if let variableOperand = knownOps[symbol]{
+                opStack.append(variableOperand)
+            }
+            return evaluate()
+        }
     
-    func pushOperand(operand: Double) -> Double? {
-        opStack.append(Op.operand(operand))
+       func pushOperand(operand: Double) -> Double? {
+            opStack.append(Op.operand(operand))
         
         return evaluate()
     }
@@ -67,6 +99,11 @@ class BrainCalc{
             switch op {
             case .operand(let operand):
                 return (operand, remainingOps)
+            case .variable(_, let operation):
+                return (operation(), remainingOps)
+            case .operandPi(_,let operation):
+                return (operation(), remainingOps)
+            
             case .UnaryOperation(_, let operation):
                 let operandEvaluation = evaluate(remainingOps)
                 if let operand = operandEvaluation.result{
@@ -88,6 +125,68 @@ class BrainCalc{
     func evaluate() -> Double? {
         let (result, remainder) = evaluate(opStack)
         print("\(opStack) = \(result) with \(remainder) left over")
+        print ("\(description)")
         return result
     }
+    
+    private func evaluate1(ops: [Op]) -> (result: String? , remainingOps: [Op]){
+        var resultString = String()
+        var remainingOps = ops
+        if !ops.isEmpty {
+            let op = remainingOps.removeLast()
+            switch op {
+            case .operand(let operand):
+                if operand == M_PI{
+                    return ("", remainingOps)
+                }
+                return ("\(operand)" , remainingOps)
+            case .variable(let variable , _):
+                return (variable , remainingOps)
+            case .operandPi(let operandPi , _):
+               return (operandPi , remainingOps)
+            case .UnaryOperation(let operate , _ ):
+                let operandEvaluation = evaluate1(remainingOps)
+                if let operand = operandEvaluation.result{
+                    resultString = operate + "(" + operand + ")"
+                }
+                else{
+                    resultString = operate + "?"
+                }
+                return (resultString , operandEvaluation.remainingOps)
+            case .BinaryOperation(let operate,_):
+                let op1Evaluation = evaluate1(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    if operand1 != ""{
+                        if operand1 != ""{
+                            resultString = operand1
+                            let op2Evaluation = evaluate1(op1Evaluation.remainingOps)
+                            if let operand2 = op2Evaluation.result{
+                                if operand2 != ""{
+                                    resultString = "(" + operand2 + operate + operand1 + ")"
+                                }
+                                else{
+                                    resultString = operand1 + "?" + ")"
+                                }
+                            }
+                            return (resultString, op2Evaluation.remainingOps)
+                        }
+                    }
+                    else
+                    {
+                       resultString = "(" + "?" + "\(operate)" + "?" + ")"
+                        return (resultString, op1Evaluation.remainingOps)
+                    }
+                }
+                else{
+                    return (resultString, remainingOps)
+                    
+                }
+            }
+        }
+        return (resultString, remainingOps)
+    }
+    func clearOp(){
+        opStack.removeAll()
+    }
+
 }
--- BrainCalc/ViewController.swift
+++ BrainCalc/ViewController.swift
@@ -9,36 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-//MARK: -global var-
+//MARK: -global var and let-
+    let decimalSeparator = NSNumberFormatter().decimalSeparator ?? "."
     var isZeroOnDisplay : Bool = true
-    
+    var isPiOnRow : Bool = false
     var brainCalculator = BrainCalc()
     var operandStack = Array <Double> ()
-    var displayValue : Double{
+    var displayValue : Double?{
     
         get{
-            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
-        
+            if let displayText = display.text{
+                if displayText != " "{
+                
+                    return NSNumberFormatter().numberFromString(displayText)!.doubleValue
+                }
+            }
+            return nil
         }
         
         set{
-            display.text = "\(newValue)"
-            isZeroOnDisplay = true
+            if newValue != nil{
+                display.text = "\(newValue!)"
+                isZeroOnDisplay = true
+            }
+            else{
+                display.text = " "
+                //historyRow.text = historyRow.text! + "error"
+            }
         
         }
     }
     
 //MARK: -Outlets-
     @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var decimalSeparatorLocation: UIButton!
+    @IBOutlet weak var historyRow : UILabel!
  
 //MARK: -Action-
     @IBAction func apendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
+        
+        if (digit == decimalSeparator) && (display.text?.rangeOfString(decimalSeparator) != nil)
+            {return}
+
+
         if isZeroOnDisplay{
-            if digit != "0"{
-                display.text = digit
-                isZeroOnDisplay = false
-            }
+                if (digit != "0" && digit != "00" && digit != "000") || (display.text != "0"){
+                    display.text = digit
+                    isZeroOnDisplay = false
+                }
         }
         else{
             display.text = display.text! + digit
@@ -46,13 +65,25 @@ class ViewController: UIViewController {
     }
     
     @IBAction func enter() {
-        isZeroOnDisplay = true
-        
-        if let result = brainCalculator.pushOperand(displayValue){
-            displayValue = result
-        }else{
-            //error?
-            displayValue = 0 //задание 2
+        //isZeroOnDisplay = true
+        //historyRow.text = historyRow.text!
+        if isPiOnRow{
+            isZeroOnDisplay = true
+            isPiOnRow  = false
+            addHistory(brainCalculator.description!)
+        }
+        else{
+            if let displayVal = displayValue{
+                if let result = brainCalculator.pushOperand(displayVal){
+                    addHistory(brainCalculator.description!)
+                    //isPiOnRow  = false
+                    isZeroOnDisplay = true
+                    displayValue = result
+                }else{
+                    isZeroOnDisplay = false
+                    displayValue = nil
+                }
+            }
         }
     }
     
@@ -61,30 +92,57 @@ class ViewController: UIViewController {
             enter()
         }
         if let operation = sender.currentTitle{
+            if sender.currentTitle == "π"{
+                isPiOnRow = true
+            }
             if let result = brainCalculator.performOperation(operation){
                 displayValue = result
             }else{
-                //error?
-                displayValue = 0
+                displayValue = nil
             }
+            addHistory(brainCalculator.description! + "=")
         }
         
-//        switch operation {
-//        case "×":performOperation {$0 * $1}
-//        case "÷":performOperation {$1 / $0}
-//        case "-":performOperation {$1 - $0}
-//        case "+":performOperation {$0 + $1}
-//        case "√":performOperation {sqrt($0)}
-//        default:
-//            NSLog("Error in operate(sender:\(sender.currentTitle))")
-//            break
-        
+    }
+    @IBAction func backSpace(sender : UIButton){
+        if(!isZeroOnDisplay){
+            if  (display.text!).characters.count > 1{
+                 display.text!.removeAtIndex( display.text!.endIndex.predecessor())
+            }
+            else{
+                display.text = "0"
+                isZeroOnDisplay = true
+            }
+        }
     }
     
+    @IBAction func clearAll(sender : UIButton){
+        display.text? = "0"
+        historyRow.text = " "
+        isZeroOnDisplay = true
+        isPiOnRow  = false
+        operandStack.removeAll()
+        brainCalculator.clearOp()
+        brainCalculator.variableValues.removeAll()
+    }
+    @IBAction func pushVariableM(sender : UIButton){
+        if isZeroOnDisplay == false && display.text! != "0"{
+            brainCalculator.variableValues["M"] = displayValue!
+            displayValue =  brainCalculator.evaluate()
+            isZeroOnDisplay = true
+        }
+    }
     
+    @IBAction func popVariableM(sender : UIButton){
+ 
+               brainCalculator.pushOperand("M")
+        addHistory(brainCalculator.description! + "=")
+               //enter()
+    }
 //MARK: -Override metods-
     override func viewDidLoad() {
         super.viewDidLoad()
+        decimalSeparatorLocation.setTitle(decimalSeparator, forState: .Normal)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -97,20 +155,8 @@ class ViewController: UIViewController {
     
 //MARK: -Metods-
     
-    func performOperation(operation:(Double, Double)->Double){
-        if operandStack.count >= 2{
-            displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    //@nonobjc
-    
-   private func performOperation(operation:(Double)->Double){
-        if operandStack.count >= 1{
-            displayValue = operation(operandStack.removeLast())
-            enter()
-        }
+    func addHistory(str : String){
+            historyRow.text = str
     }
 
 }
